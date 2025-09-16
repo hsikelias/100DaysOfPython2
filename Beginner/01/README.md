90d6860a7beebe16ea4cbb94acbc62120d91812b
Calamity <plekishsai@gmail.com>
2025-09-16 15:53:50 +0000

    Day 1 notes

---
+++ Beginner/01/README.md
@@ -0,0 +1,3 @@
+Day: 1
+Concepts learned: Working with variables, input, output
+Program built: The program comes up with a bandname with user's city they grew up in and their pet name.
--- Beginner/01/notes.py
+++ Beginner/01/notes.py
@@ -1,1 +1,22 @@
 
+print("What's up")
+
+# this is a print function,  what u want to print is between the double quotes.
+# the key word here is print
+
+# adding things inside the double quotes tells the computer that whatever is inside it
+# is not a code, its called STRING. Double quotes show the beginning and end of the string.
+
+# when you face an error, just copy paste it to google and use sources like stackoverflow to get answers. 
+# Every porgrammer makes a mistake, you just have to be good at figuring out the problem.
+
+# not just double quotes we can also use single quotes
+
+print('"Hello"')
+print("Hey's")
+
+#\n This moves the next string to new line
+print("Yeezy season approachin \nfk what yall been hearin")
+
+# concataate strings
+print("Gurt:"+" yo")
