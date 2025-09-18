a7ca7dce06a345bc5944de3fd11df079049be6e6
Calamity <plekishsai@gmail.com>
2025-09-18 14:21:04 +0000

    day 2 kinda done

---
+++ Beginner/02/notes.py
@@ -0,0 +1,32 @@
+print(len("true"))
+
+# what if I want to caluclate the len in input. If we run 
+# print(len(1245)), we get a data type error.
+
+
+# DATA TYPES
+
+# "HELLO" == string
+
+"Hello"[0] # we can access any position in a string, output = H, this is called SUBSCRIPTING
+#01234
+
+# 123 is not the same as "123"
+print("123"+"345")
+# INTEGERS
+print(123 +345)
+print(123456)
+print(12_34_45) # _ is getting ignored
+
+# FLOAT: has decimal points
+print(3.35234)
+
+# BOOLEAN: Only two values, true/false
+True
+False
+
+# -------------------------------------------------------------------------------------------------------------------------------------
+
+# TYPE ERROR, TYPE CHECKING AND TYPE CONVERSION
+
+len()
