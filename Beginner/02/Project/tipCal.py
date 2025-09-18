e09e8ce8f0d5f2f477cb2191e43e46797c6a2d0e
Calamity <plekishsai@gmail.com>
2025-09-18 16:03:15 +0000

    Fixed some small bugs in day 2 project, now theres accurate calculation of the bill

--- Beginner/02/Project/tipCal.py
+++ Beginner/02/Project/tipCal.py
@@ -1,9 +1,11 @@
 print("Welcome to the Tip Caluclator")
-bill = int(input("Enter the total bill amount: $"))
+bill = float(input("Enter the total bill amount: $"))
 tip = int(input("How much tip would you like to give?: $"))
 split = int(input("How many people are splitting the bill?: "))
 
-totalBill = bill + tip
-onePersonBill = totalBill/split
+tip_percent = tip/100
+total_tip = bill*tip_percent
+total_bill = total_tip + bill
+onePersonBill = total_bill/split
 
-print(f"Each person should pay ${onePersonBill}")
+print(f"Each person should pay ${round(onePersonBill)}")
