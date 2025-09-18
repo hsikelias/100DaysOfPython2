3cd1ec54d80414b3758b4b31cabcbb15993f85a0
Calamity <plekishsai@gmail.com>
2025-09-18 13:54:11 +0000

    Day 1 completed

--- Beginner/01/Projects/BandNameGen.py
+++ Beginner/01/Projects/BandNameGen.py
@@ -1,5 +1,6 @@
+print("Welcome to the Band Name Generator")
 cityName = input("Enter your city name: ")
 petName = input("Enter your pet name: ")
 
-bandName = cityName+ petName
+bandName = cityName +" "+petName
 print(f"Your band name is", bandName )
--- Beginner/01/notes.py
+++ Beginner/01/notes.py
@@ -15,8 +15,48 @@ print("What's up")
 print('"Hello"')
 print("Hey's")
 
+print("Hello World")
+print("Hello World")
+print("Hello World")
+
+# This takes too much space just for wanting to print in a new line, we can use \n to create a new line. 
+
 #\n This moves the next string to new line
 print("Yeezy season approachin \nfk what yall been hearin")
 
 # concataate strings
 print("Gurt:"+" yo")
+print("Hello"+" Whats up")
+
+# space is very important in python, you will face an Indentation error in such cases.
+# sometimes there might not be an issue in indentation but in complex programs involving if/else/loops its important
+# to understand where to place your code. 
+
+
+# you get a syntax error if you make any mistakes like forgeting to close a string statement  with "
+
+
+# INPUT: we use this when we want the user to enter some data. Instead of print, which we enter the data our self.. we use input to
+# ask the user to enter something
+# - Inputs are string at default
+
+input("What is your name: ")
+
+# currently the input function does ask the user for data, but we're not using it anywhere yet
+
+print("Hello "+ input("What is your name: ")+ "!")
+# another use of string concatination. Now we can greet the user
+
+
+# VARIABLES: before we did ask the user for a name, but it just ends with asking the name..
+# we use variables to actually refer to that data later.
+
+username = input("Whats your name ")
+len_username = len(username) #so now we can use name data for whatver and whenever we want
+
+print(username)
+print(len_username)
+
+# Variable Naming
+
+# be clear with how you name variables.
