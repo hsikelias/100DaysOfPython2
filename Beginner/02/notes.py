f3983a47cb14997715dd22459595311cfed393d1
Calamity <plekishsai@gmail.com>
2025-09-18 15:37:15 +0000

    Day2 notes completed, yet to do the final day project

--- Beginner/02/notes.py
+++ Beginner/02/notes.py
@@ -29,5 +29,40 @@ False
 
 # TYPE ERROR, TYPE CHECKING AND TYPE CONVERSION
 
-len()
-#TEST
+# To check data type:
+print(type("hello"))
+print(type(True))
+print(type(123))
+print(type(234.3))
+
+
+# "123" this is a string right now
+
+print("123"+"456")
+
+#converting the string to int
+print(int("123")+int("456"))
+
+#but u cant always convert something to another data type
+
+# print(int("abc")+int("def"))   This gives a value error
+
+
+print("Number of letters in your name: " , len(input("Enter your name")))
+# we're not using + because one part is a string and the other is an int
+
+#Round
+
+bmi = 84/1.65 **2
+print(bmi)
+print(round(bmi))
+
+# reassigning values
+
+score = 0
+score -= 3
+print(score)
+
+# f strings: Used for mixing strings with data types
+
+print(f"Hello, you scored {score} points, your bmi is {int(bmi)}")
--- README.md
+++ README.md
@@ -11,6 +11,7 @@ This repository is inspired by [@Matthew1906](https://www.github.com/Matthew1906
 # Coruse Content:
 
 * [Day 1: Working with variables, input, output](Beginner/01/)
+* [Day 2: Data Types,roundings,f-strings,string Manipulation](Beginner/02/)
 
 
 
