d747d6c82c1d7426ea429c420fcc9565984eeb79
hsikelias <plekishsai@gmail.com>
2025-09-30 08:44:51 -0400

    Day 6 start

---
+++ Beginner/06/notes.py
@@ -0,0 +1,12 @@
+# FUNCTIONS
+
+print() # functions are followed by ()
+print("Hello World")
+
+# but, how to make own functions?
+
+# start with def key word, it defines its the function
+
+def my_function():
+  print("Hello")
+  print("Bye Bye")
