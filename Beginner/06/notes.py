5e2be3414cb3db21644fb252828022d1389a727d
Calamity <plekishsai@gmail.com>
2025-09-30 14:57:01 +0000

    day 6 half done, saved reeborg code in the text file

--- Beginner/06/notes.py
+++ Beginner/06/notes.py
@@ -10,3 +10,27 @@ print("Hello World")
 def my_function():
   print("Hello")
   print("Bye Bye")
+
+my_function()
+
+# Defining Functions
+
+# def do_thisfunction():
+
+   # DO THIS
+   # THEN THIS
+   # FINALLY THIS
+
+# calling the function
+   # do_thisfunction()
+
+
+# WHILE LOOPS
+
+# a loop that continues to happen while the condition is true or it just runs infinitely no matter what.
+
+while 2 == 2:
+  print("2 + 2 is 5 fight me")
+
+# for loops are great if you want to iterate over individual things in a list.
+# while loops do things repeatedly, if you're not careful u might end up with a infinite loop.
