29a33893202339b9e8fb9e3b4b79d52004afd662
hsikelias <plekishsai@gmail.com>
2025-10-06 20:02:00 -0400

    day 7 completed

--- Beginner/07/Project/hangman.py
+++ Beginner/07/Project/hangman.py
@@ -130,23 +130,36 @@ words = [
 lives = 8
 chosen_word = random.choice(words)
 blank_word = ["_" for _ in chosen_word]
+gussed_letters = []
 
 
 print(logo)
-
-print("Welcome to Hangman! Below is your word, start guessing letters!!\n")
-
-print(blank_word)
-print("\n")
-print(chosen_word)    # REMOVE THIS LATER, RN ITS JUST HERE FOR DEBUGGING
-
-print(symbol[lives])
-guess = input("Enter your letter guess:").lower()
+print("Welcome to Hangman! Below is your word, start guessing letters!\n")
 
 while lives > 0:
-	for letter in chosen_word:
-		if letter == guess: 
-            print('Nice guess!')
-        else:
-            lives -= 1
-            print(symbol[lives])
+    print(" ".join(blank_word))
+    print(symbol[8 - lives]) 
+
+    guess = input("\nEnter a letter: ").lower()
+
+    if guess in gussed_letters:
+        print("You already guessed that letter.")
+        continue
+    gussed_letters.append(guess)
+
+    if guess in chosen_word:
+        for i, letter in enumerate(chosen_word):
+            if letter == guess:
+                blank_word[i] = guess
+        print("Nice guess!")
+    else:
+        lives -= 1
+        print("Wrong guess!")
+    
+    if "_" not in blank_word:
+        print(f"\nYou won! The word was '{chosen_word}' indeed.")
+        break
+
+else:
+    print(symbol[-1])
+    print(f"\n You lost! The word was '{chosen_word}'.")
