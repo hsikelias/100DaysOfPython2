83ec9a550ce7e27c74e6e8e67ef8b9d28aa2197f
hsikelias <plekishsai@gmail.com>
2025-10-21 10:27:08 -0400

    Day 7 continued

--- Beginner/07/Project/hangman.py
+++ Beginner/07/Project/hangman.py
@@ -1,35 +1,46 @@
 import random
-
+art = []
 word_list = ["aardvark", "baboon", "camel"]
-pickedWord = random.choice(word_list)
-lives = 7
-print(pickedWord)
 
-hiddenWord = "_"*len(pickedWord) # lesgo we got the words replaced to _
-hiddenList = list(hiddenWord)  # converting the hidden word, whichs in a string to a list. we do this cus lists makes it easier to update blanks and more control basically.
+chosen_word = random.choice(word_list)
+lives = 6
+display = ""
 
 print("Welcome to hangman!")
-print("Below is your hidden word, start guessing, one letter at a time!")
-print(hiddenWord) 
-print(hiddenList)  
+print("Below is your word, start guessing, one letter at a time!")
 
-while lives >0 and "_"  in hiddenList:
-  guess = input("Guess a letter: ")
-  if guess in pickedWord:
-    enumerate(pickedWord)
-    print("Your guessed letter is correct!")
-  elif guess == hiddenList:
-    print("Entered guess is already guessed, try a new letter!")
-  elif guess != hiddenList:
-    print("Wrong guess")
-  else:
-    print("You entered "+ guess+",you might've entered a number, please enter only one letter!")
-
-# need to convert the hidden word variable to list. 
-# so with this i get the position of the letter but in the case of the multiple characters it doesnt give out every single character. like in baboon.. if i say b it doesnt give out 0,2.. only 0.
-# so this method is really awful because i want the user to get all the existing letters.
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+  placeholder += "_" 
+print(placeholder)
 
 
-    print(hiddenList)
-  
+game_over = False
+correct_letters = []
+
+while not game_over:
+  guess = input("Guess a letter: ").lower()
+
+  display = ""  
+
+  for letter in chosen_word:
+    if letter == guess: 
+      display += letter
+      correct_letters.append(letter)
+    elif letter in correct_letters:
+      display += letter
+    else:
+      display += "_"
+      lives -= 1
+
+  print(display)
+
+  if "_" not in display:
+    game_over = True 
+    print("You win!")
+
+
+
+  # so now we gotta use whiles to loop thru a logic, in the while loop the program should repeat the process of prompting user to enter the word till either there are no "_" and lives is above 0
 
