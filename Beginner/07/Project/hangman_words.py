83d77cd8dad9b9d59c479fd1f0ff41320d6d3e15
Calamity <plekishsai@gmail.com>
2025-10-21 14:53:07 +0000

    Day 7 done LFG

---
+++ Beginner/07/Project/hangman_words.py
@@ -0,0 +1,5 @@
+word_list = ["apple", "banana", "cherry", "date", "elephant", "fox", "grape", "house", "igloo", "jungle",
+        "kite", "lemon", "mountain", "notebook", "ocean", "pencil", "quilt", "river", "sun", "tiger",
+        "umbrella", "vase", "whale", "xylophone", "yacht", "zebra", "ant", "ball", "cat", "dog",
+        "eagle", "frog", "goat", "hat", "ice", "jacket", "kangaroo", "lamp", "moon", "nest", "orange",
+        "penguin", "queen", "rain", "star", "tree", "unicorn", "violet", "wolf", "x-ray", "yak"]
