9b013b474e9e841dd253535c6e365440ba07622e
hsikelias <plekishsai@gmail.com>
2025-10-29 22:02:27 -0400

    Day 9 Completed

--- Beginner/09/Project/auction.py
+++ Beginner/09/Project/auction.py
@@ -6,17 +6,17 @@ print("Welcome to the Auction! Start Bidding!")
 other_bidders = "yes"
 bids = { }
 
-# TODO-1: Ask the user for input
 while other_bidders == "yes":
   name = input("Enter your name: ")
   price = float(input("Enter your bid amount: $"))
-# TODO-2: Save data into dictionary {name: price}
+
   bids[name] = price
   print("\n"*100)
-  print(bids)
-# TODO-3: Whether if new bids need to be added
+
   other_bidders = input("Are there any other bidders? Type 'yes' or 'no': ").lower()
 
+key_value = max(bids.items(),key=lambda item: item[1])
+winner_name = key_value[0]
+winner_amount = key_value[1]
 
-
-# TODO-4: Compare bids in dictionary
+print(f"The highest bid was $ {winner_amount} bidded by {winner_name}. Congratulations!!")
