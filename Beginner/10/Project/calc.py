658a61191bfd8bfa5e0e6d5d7c5d40cba34e37f7
Calamity <plekishsai@gmail.com>
2025-11-06 20:28:14 +0000

    Day 10 kinda complete small bugs

--- Beginner/10/Project/calc.py
+++ Beginner/10/Project/calc.py
@@ -5,7 +5,6 @@ print(logo)
 # program starts with asking the user first number and then displays bunch of operations( +, -, *, /). Then it asks for user to enter the operation.
 # Then the program asks user for another number. After entering the number the program pritns the caluclation and answer. 
 # Then a new prompt which asks y to save the previous answer for next caluclation or n for starting a new caluclation. 
-
 def mainCalc (fNum, opr, sNum):
   if opr == "+":
     answer = fNum + sNum
@@ -17,15 +16,25 @@ def mainCalc (fNum, opr, sNum):
     answer = fNum / sNum
   else:
     print("Make sure you entered valid operations!")
+
   print(answer)
 
+  option = input("Do you want to continue? y for yes and n for no: ")
+  if option == "y":
+    answer = firstNum
+  else:
+    choice = "n"
+
+
+
+firstNum = int(input("Enter your first number: "))
+operation = input(f"Choose one: \n + \n - \n * \n / \n")
+secondNum = int(input("Now enter your second number: "))
+mainCalc(firstNum,operation,secondNum)
+
 while choice == "y":
-  firstNum = int(input("Enter your first number: "))
   operation = input(f"Choose one: \n + \n - \n * \n / \n")
   secondNum = int(input("Now enter your second number: "))
-
   mainCalc(firstNum,operation,secondNum)
 
-  option = input("Do you want to continue? y for yes and n for no: ")
-  if option != "y":
-    choice = "n"
+
