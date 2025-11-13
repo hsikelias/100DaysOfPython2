fe1a444346656d51a33114b1296de70166877744
Calamity <plekishsai@gmail.com>
2025-11-13 18:58:30 +0000

    THE CALUCLATOR WORKSS

--- Beginner/10/Project/calc.py
+++ Beginner/10/Project/calc.py
@@ -1,48 +1,46 @@
 from art import logo
 print(logo)
 
-
-def mainCalc (fNum, sNum,opr):  
+def mainCalc (fNum, sNum,opr,choice):  
     """
     This function does the job of completing the selected operation and then have a loop that reuses the first num value to repeat the first number.  
+    The function has bunch of if statements to do the operation and then we will do a while loop 
+    
     """ 
+    while choice == "y":
+        if opr == "+":
+            answer = fNum + sNum
+        elif opr == "-":
+            answer = fNum - sNum
+        elif opr == "*":
+            answer = fNum * sNum
+        elif opr == "/":
+            answer = fNum / sNum
+        else:
+            print(f"Enter only provided operations, you responded {opr}")
+
+        print(f"Your answer is {answer}")
+
+        print("Do you want to continue the game?\n")
+        continueOrNot = input("Type 'y' if you want to continue and 'n' if you want to exit. ").lower()
+
+        if continueOrNot == "y":
+            print("+ \n- \n* \n/ \n")
+            opr =input("Now, please enter the operation you want to do: ")
+            sNum = int(input("Please enter your second number: "))
+            fNum = answer
+            choice = "y"
+        else:
+            choice = "n"
+            exit()
     
-    if opr == "+":
-        answer = fNum + sNum
-    elif opr == "-":
-        answer = fNum - sNum
-    elif opr == "*":
-        answer = fNum * sNum
-    elif opr == "/":
-        answer = fNum / sNum
-    else:
-        print("Print given operations")
-    # completes first iternation
-    print(f"Your answer is {answer}")
-
-    print("Do you want to continue the game?\n")
-    continueOrNot = input("Type 'y' if you want to continue and 'n' if you want to exit. ")
-    if continueOrNot == "y":
-        print("Alright lets do this again..")
-    else:
-        exit()
-    return answer
-    
-
-def secondMainCalc (fNum, sNum,opr,choice):
-    print("test")
-
-
+    # PROGRAM WORKS PROPERLY, well kind of.. the problem is that when user enters y it doesnt ask the user for a new operation and second number. 
+choice = "y"
 
 firstNum = int(input("Please enter your first number: "))
 print("+ \n- \n* \n/ \n")
 operation =input("Now, please enter the operation you want to do: ")
 secondNum = int(input("Please enter your second number: "))
 
-mainCalc(firstNum,secondNum,operation)
-# atp user will get the answer and its time to ask if they want to continue
+mainCalc(firstNum,secondNum,operation,choice)
 
-print("Do you want to continue the game?\n")
-continueOrNot = input("Type 'y' if you want to continue and 'n' if you want to exit. ")
-if continueOrNot == "y":
-    choice = continueOrNot
