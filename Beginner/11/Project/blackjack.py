95ad3ceda9ea6cbecbcfc8871c5a00a4f4a7410d
hsikelias <plekishsai@gmail.com>
2025-11-23 22:59:05 -0500

    black jack continue tmr

--- Beginner/11/Project/blackjack.py
+++ Beginner/11/Project/blackjack.py
@@ -1,34 +1,40 @@
-'''
-You have bunch of cards, user chooses wether to hit or stand, u repeat asking if they want to hit or stand while the card amount is below or = to 21.
-If the card amount crosses 21, bidder wins! each caard has its own value. 
-from 2 - 10, count as their face values so 6 is 6, 9 is 9, but jack, queen, and king each count as 10. Ace can be either counted as 1 or 11, u choose which value
-ace to represent. 
-You can't see dealers card, dealer must take another card till they pass 17. If yuu and dealer have same amount of card value then its a draw. 
+# https://appbrewery.github.io/python-day11-demo/
 
-Our game: 
-
-cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
-// counted as 11 only if the overall user card value goes over 21
-
-The card is not removed
-
-things in the final project:
-current score tracker
-list of my cards
-orginal cards
-showing computers card
-computer wont stop until u bust
-
-important website: https://appbrewery.github.io/python-day11-demo/
-'''
-# import necessary libraries
 
 import random
 from art import logo
 
-def clearScreen():
-    print("\n"*100)
-    print(logo)
+print(logo)
+cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
 
-print("hello world")
-clearScreen()
+def cardsDealer():
+
+  """
+  I do the job of randomizing cards and assigning them to the users cards and computer cards. I also update the total count of computer and user cards. 
+  """
+
+def gameLogic():
+
+  """
+  I take care of the game logic, that involves tracking the total sum count of user and then determining if they lose or not. If user stands then I take care of the bidders logic, I keep adding randomized cards to the bidders cards till the count reach equal to or above 17. If user count and bidder count is equal, i determine the draw logic. 
+  """
+
+def game():
+
+  """
+  Asks user if they want to play the game, call this function at the beginning of the game and after the user completes the game. 
+  """
+
+  userPlay = input("Do you want to play a game of black jack? Type 'y' or 'n': ").lower()
+  if userPlay == "y":
+    print("Alright lesgo!")
+    # cardsDealer()
+  elif userPlay == "":
+    game()
+  else:
+    print("Thanks for visiting us!")
+    exit()
+
+# next steps for me is to randomize the cards from [cards] and create a list of user cards, total score calc, 
+
+game()
